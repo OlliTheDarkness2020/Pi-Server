7fa43694cb875ed95d156953d43313debc8ae192
Olli <67396171+OlliTheDarkness2020@users.noreply.github.com>
2024-08-04 17:33:28 +0200

    Edit to local file

--- src/config.h
+++ src/config.h
@@ -7,15 +7,15 @@
 
 #define PACKAGE            "piserver"
 
-#define PISERVER_HOSTSFILE  PISERVER_DATADIR "/hosts.json"
+#define PISERVER_HOSTSFILE PISERVER_DATADIR "/hosts.json"
 #define PISERVER_DISTROFILE PISERVER_DATADIR "/installed_distros.json"
 #define PISERVER_SETTINGSFILE PISERVER_DATADIR "/settings.json"
-#define PISERVER_TFTPROOT   PISERVER_DATADIR "/tftproot"
+#define PISERVER_TFTPROOT PISERVER_DATADIR "/tftproot"
 #define PISERVER_DISTROROOT PISERVER_DATADIR "/os"
 #define PISERVER_SHAREDROOT PISERVER_DISTROROOT "/shared"
-#define PISERVER_HOMEROOT   "/home"
+#define PISERVER_HOMEROOT "/home"
 #define PISERVER_DNSMASQCONFFILE "/etc/dnsmasq.d/piserver"
-#define PISERVER_REPO_URL  "/var/lib/piserver/piserver.json"
+#define PISERVER_REPO_URL PISERVER_DATADIR "/imageserver.json"
 #define PISERVER_REPO_CACHEFILE PISERVER_DATADIR "/avail_distros_cache.json"
 #define PISERVER_POSTINSTALLSCRIPT PISERVER_DATADIR "/scripts/postinstall.sh"
 #define PISERVER_CONVERTSCRIPT PISERVER_DATADIR "/scripts/convert_folder.sh"
@@ -24,10 +24,10 @@
 #define DHCPANALYZER_FILTER_STRING "PXEClient"
 
 #define PISERVER_LDAP_URL  "ldapi:///"
-#define PISERVER_DOMAIN    "raspberrypi.local"
-#define PISERVER_LDAP_DN   "dc=raspberrypi,dc=local"
+#define PISERVER_DOMAIN    "raspberry-server.local"
+#define PISERVER_LDAP_DN   "dc=raspberry-server,dc=local"
 
-#define PISERVER_LDAP_USER "cn=admin,dc=raspberrypi,dc=local"
+#define PISERVER_LDAP_USER "cn=admin,dc=raspberry-server,dc=local"
 
 #define PISERVER_LDAP_TIMEOUT  10
 #define PISERVER_MIN_UID   10000
