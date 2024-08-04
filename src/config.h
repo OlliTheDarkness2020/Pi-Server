b39807f73f81a5b8659938b4118ed6781edaa1d9
Olli <67396171+OlliTheDarkness2020@users.noreply.github.com>
2024-08-04 07:36:24 +0200

    Update config.h
    Switch file piserver.json from online to a local file.

--- src/config.h
+++ src/config.h
@@ -15,7 +15,7 @@
 #define PISERVER_SHAREDROOT PISERVER_DISTROROOT "/shared"
 #define PISERVER_HOMEROOT   "/home"
 #define PISERVER_DNSMASQCONFFILE "/etc/dnsmasq.d/piserver"
-#define PISERVER_REPO_URL  "http://downloads.raspberrypi.org/piserver.json"
+#define PISERVER_REPO_URL  "/var/lib/piserver/piserver.json"
 #define PISERVER_REPO_CACHEFILE PISERVER_DATADIR "/avail_distros_cache.json"
 #define PISERVER_POSTINSTALLSCRIPT PISERVER_DATADIR "/scripts/postinstall.sh"
 #define PISERVER_CONVERTSCRIPT PISERVER_DATADIR "/scripts/convert_folder.sh"
